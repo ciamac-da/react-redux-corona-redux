574a19926ef0cd822b38e8cf43fa60dd7dc05976
cia-from-earth <53003092+ciamac-da@users.noreply.github.com>
2020-03-12 13:08:18 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -15,18 +15,42 @@ const defaultState ={
       ]
 };
 
+function patientHinzufügen(state){
+      const welcheListe = state.testPositiv ? 'positiv' : 'negativ';
+      // Mit array.push würde Redux nicht verstehen, dass der Array sich geändert hat.
+      const neueListe = [...state[welcheListe], state.name]; 
+      return {...state, [welcheListe]:neueListe};
+}
+
+
 function reducer ( state = defaultState, action){
+      const { type, name, liste, index } = action;
       switch( action.type ){
-           case "nameÄndern": break;
-           case "testergebnisUmschalten": break;
-           case "patientHinzufügen": break;
-           case "patientLöschen": break;
-           
+           case "nameÄndern":             state = { ...state, name }; break;
+           case "testergebnisUmschalten": state = { ...state, testPositiv: ! state.testPositiv }; break;
+           case "patientHinzufügen":      state = patientHinzufügen(state); break;
+           case "patientBearbeiten":      state = { ...state, }; break;          
+           case "patientLöschen":           
+           // Erstelle eine Kopiew von der Liste, aus welcher wir den Patienten löschen wollen.
+           const neueListe = [...state[liste] ]; // const neueListe = state[liste].splice
+           neueListe.splice( index, 1);
+           state = { ...state, [liste]: neueListe };
+           break;
            default:
       }
       return state;
 }
 
+function mapActionsToProps ( dispatch ){
+      return{
+            testergebnisUmschalten:             ()=>{ dispatch({ type:"testergebnisUmschalten"           })},
+            patientHinzufügen:                  ()=>{ dispatch({ type:"patientHinzufügen",               })},
+            nameÄndern:                     (name)=>{ dispatch({ type:"nameÄndern",            name      })},
+            patientLöschen:         (liste, index)=>{ dispatch({ type:"patientLöschen",     liste,index  })},
+            patientBearbeiten:      (liste, index)=>{ dispatch({ type:"patientBearbeiten",  liste,index  })},
+
+      }
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
