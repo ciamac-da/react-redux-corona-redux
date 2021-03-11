309e024bff901e4e198a2322bebe51592335b9d3
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 14:21:59 +0100

    Updates

---
+++ precache-manifest.9f2666750f3a1bb401bd05d68874213a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15864c5f62b349af1a09ff06f93f1251",
+    "url": "/react-reduc-corona-redux/index.html"
+  },
+  {
+    "revision": "e61e0bdc688442667a01",
+    "url": "/react-reduc-corona-redux/static/css/main.52b195ae.chunk.css"
+  },
+  {
+    "revision": "7635aaf9433cbfb6702a",
+    "url": "/react-reduc-corona-redux/static/js/2.29ca5ffd.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-reduc-corona-redux/static/js/2.29ca5ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61e0bdc688442667a01",
+    "url": "/react-reduc-corona-redux/static/js/main.5f5ea206.chunk.js"
+  },
+  {
+    "revision": "275a427b1a3f4f4bc65f",
+    "url": "/react-reduc-corona-redux/static/js/runtime-main.3a64e80d.js"
+  }
+]);
